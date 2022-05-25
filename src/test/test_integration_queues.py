2a7c21bf703177697cac040aaaf80d22cb1d809a
ParthZala <p.zala@logabit.com>
2022-05-25 23:31:04 +0530

    Remove unnecessary files, and also update the test_integration_queue file as every test will have different method

--- src/test/test_integration_queues.py
+++ src/test/test_integration_queues.py
@@ -1,13 +1,26 @@
-#This is the integration test file which i'm working on it now
+import requests
+from requests.auth import HTTPBasicAuth
+BASE_URL = "http://172.24.160.1:15672"
+API = BASE_URL + "/api/queues"
 
-import sys
-sys.path.insert(1, 'D:/Logabit/pipeforce-service-template-python-1/src/service')
-sys.path.append("../service/")
-import main
+def test_status_of_rabbitmq():
+    response = requests.get(
+        "http://localhost:15672",
+        auth=HTTPBasicAuth('guest', 'guest')
+    )    
+    assert response.status_code == 200    
 
+def test_exists():
+    response  = requests.get(
+        API + '/foo/2nd Key',
+        auth=HTTPBasicAuth('guest', 'guest')
+    )    
+    assert response.status_code == 200
 
-def test_statuscheck():
-    assert main.statuscheck() == 200
-    assert main.existcheck() == 200
-    assert main.statusofqueue == 'running'
-
+def test_status_of_queue():
+    response = requests.get(
+        API + '/foo/2nd Key',
+        auth=HTTPBasicAuth('guest', 'guest')
+    )   
+    resp = response.json()    
+    assert resp['state'] == "running"
