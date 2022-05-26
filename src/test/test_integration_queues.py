880443625f34c38dbc14458b0a84ea4a32e9d17a
ParthZala <p.zala@logabit.com>
2022-05-26 19:42:56 +0530

    Now it is running in my local container and working fine

--- src/test/test_integration_queues.py
+++ src/test/test_integration_queues.py
@@ -1,26 +1,46 @@
 import requests
+import pytest
 from requests.auth import HTTPBasicAuth
-BASE_URL = "http://172.24.160.1:15672"
-API = BASE_URL + "/api/queues"
+import os
+host = os.environ.get('PIPEFORCE_MESSAGING_HOST')
+port = os.environ.get('PIPEFORCE_MESSAGING_PORT')
+username = os.environ.get('PIPEFORCE_MESSAGING_USERNAME')
+password = os.environ.get('PIPEFORCE_MESSAGING_PASSWORD')
 
-def test_status_of_rabbitmq():
+@pytest.fixture
+def host():
+    return (os.environ.get('PIPEFORCE_MESSAGING_HOST'))
+
+@pytest.fixture
+def port():
+    return (os.environ.get('PIPEFORCE_MESSAGING_PORT'))
+
+@pytest.fixture
+def username():
+    return (os.environ.get('PIPEFORCE_MESSAGING_USERNAME'))
+
+@pytest.fixture
+def password():
+    return (os.environ.get('PIPEFORCE_MESSAGING_PASSWORD'))
+
+def test_status_of_rabbitmq(host, port, username, password):    
     response = requests.get(
-        "http://localhost:15672",
-        auth=HTTPBasicAuth('guest', 'guest')
-    )    
-    assert response.status_code == 200    
-
-def test_exists():
-    response  = requests.get(
-        API + '/foo/2nd Key',
-        auth=HTTPBasicAuth('guest', 'guest')
+         'http://'+host+':'+port,        
+        auth=HTTPBasicAuth(username, password)
     )    
     assert response.status_code == 200
 
-def test_status_of_queue():
+def test_exists(host, port, username, password):
+    response  = requests.get(
+        'http://'+host+':'+port+'/api/queues/%2F/2ndKey',
+        auth=HTTPBasicAuth(username, password)
+    )    
+    assert response.status_code == 200
+ 
+def test_status_of_queue(host, port, username, password):
     response = requests.get(
-        API + '/foo/2nd Key',
-        auth=HTTPBasicAuth('guest', 'guest')
+        'http://'+host+':'+port+'/api/queues/%2F/2ndKey',       
+        auth=HTTPBasicAuth(username, password)
     )   
     resp = response.json()    
     assert resp['state'] == "running"
